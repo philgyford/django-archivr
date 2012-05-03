432e3b1287859f6d3a382a94202539d860c6dab2
Phil Gyford <phil@gyford.com>
2012-05-03 18:16:51 +0100

    Fix Flickr photo admin

--- archivrflickr/admin.py
+++ archivrflickr/admin.py
@@ -8,7 +8,7 @@ class FlickrPhotoTagInline(admin.TabularInline):
     raw_id_fields = ('tag',)
 
 
-class FlickrPhotoAdmin(admin.OSMGeoAdmin):
+class FlickrPhotoAdmin(gis.admin.OSMGeoAdmin):
     date_hierarchy = 'posted_date'
     list_display = ('posted_date', 'show_thumb', 'title', 'owner', 'hidden',)
     list_display_links = ('title', 'show_thumb',)
@@ -17,19 +17,25 @@ class FlickrPhotoAdmin(admin.OSMGeoAdmin):
     inlines = [FlickrPhotoTagInline,]
     exclude = ('tags',)
     fieldsets = (
-        (None, {
+        ('ArchivrItem', {
+            'fields': ('order_date', 'hidden', 'coordinate', 'item_genre',)
+        }),
+        ('Photo', {
             'fields': ('flickr_id', 'owner', 'title', 'description', 'posted_date', 'updated_date', 'taken_date', 'taken_granularity', 'license', 'comments', 'visibility_is_public', 'visibility_is_friend', 'visibility_is_family',)
         }),
         ('URL etc.', {
             'fields': ('photopage_url', 'farm', 'server', 'secret', 'original_secret', 'original_format',)
         }),
         ('Image sizes', {
+            'classes': ('collapse',),
             'fields': ('large_width', 'large_height', 'largesquare_width','largesquare_height', 'medium640_width', 'medium640_height', 'medium800_width', 'medium800_height', 'medium_width', 'medium_height', 'original_width', 'original_height', 'small320_width', 'small320_height', 'small_width', 'small_height', 'square_width', 'square_height', 'thumbnail_width', 'thumbnail_height',)
         }),
-        ('Geo', {
+        ('Geo data', {
+            'classes': ('collapse',),
             'fields': ('geo_latitude', 'geo_longitude', 'geo_accuracy', 'geo_county', 'geo_county_place_id', 'geo_county_woe_id', 'geo_country', 'geo_country_place_id', 'geo_country_woe_id', 'geo_locality', 'geo_locality_place_id', 'geo_locality_woe_id', 'geo_region', 'geo_region_place_id', 'geo_region_woe_id',)
         }),
-        ('EXIF', {
+        ('EXIF data', {
+            'classes': ('collapse',),
             'fields': ('exif_aperture', 'exif_color_space', 'exif_exposure', 'exif_flash', 'exif_focal_length', 'exif_iso', 'exif_make', 'exif_metering_mode', 'exif_model', 'exif_orientation', 'exif_software',)
         }),
     )
